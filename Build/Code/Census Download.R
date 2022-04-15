c320f4f242aae35aa6e8bf1e3ad2d2955d73410f
Jeremy R. Groves <jgroves@niu.edu>
2022-04-15 16:40:52 -0500

    Completed visualization graphs and maps

--- Build/Code/Census Download.R
+++ Build/Code/Census Download.R
@@ -1,7 +1,13 @@
+#This file pulls in the Hubs and Farms and also the Census Tract maps to map the Hubs and Farms to census tract.
+#This also creates the point and spoke maps for each HUB.
+
+rm(list=ls())
+
 library(tidycensus)
 library(tidyverse)
 library(sf)
 library(readxl)
+options(tigris_use_cache = TRUE)
 
 #Functions
 
@@ -48,46 +54,109 @@ for(i in fips){
   rm(temp, map)
 }
 
+#Tracts<-rbind(illinoiscensus,iowacensus,michigancensus,wisconsincensus,missouricensus,
+#            indianacensus,minnesotacensus)
+
 State<-rbind(illinoismap_st,iowamap_st,michiganmap_st,wisconsinmap_st,missourimap_st,
               indianamap_st,minnesotamap_st)
 
 County<-rbind(illinoismap_co,iowamap_co,michiganmap_co,wisconsinmap_co,missourimap_co,
               indianamap_co,minnesotamap_co)
 
-
-
 farms<-read_excel("./Build/Input/Responses.xlsx", sheet = "Farms")
 
 farms<-farms %>%
-  filter(!is.na(Lat))
+  filter(!is.na(Lat)) %>%
+  mutate(lon1=Lon,
+         lat1=Lat)
 
-farms <- st_as_sf(farms, coords = c("Lon", "Lat"),crs = 4326, agr = "constant")
+farms.map <- st_as_sf(farms, coords = c("Lon", "Lat"),crs = st_crs(County), agr = "constant")
 
 
 hubs<-read_excel("./Build/Input/Responses.xlsx", sheet = "Hubs")
 
 hubs<-hubs %>%
-  filter(!is.na(Lat))
+  filter(!is.na(Lat)) %>%
+  mutate(lon1=Lon,
+         lat1=Lat)
 
-hubs <- st_as_sf(hubs, coords = c("Lon", "Lat"),crs = 4326, agr = "constant")
+hubs.map <- st_as_sf(hubs, coords = c("Lon", "Lat"),crs = st_crs(County), agr = "constant")
+
+temp<-hubs.map %>%
+  select(HUB)
+hub.int<-st_intersection(temp, Tracts)
+
+hub.int<-hub.int %>%
+  mutate(State=substr(GEOID,1,2),
+         County=substr(GEOID,3,5),
+         Tract=substr(GEOID,6,11)) %>%
+  select(!c(state,NAME))
 
 
-map<-ggplot()+
+temp<-farms.map %>%
+  select(Name)
+farm.int<-st_intersection(temp, Tracts)
+
+farm.int<-farm.int %>%
+  mutate(State=substr(GEOID,1,2),
+       County=substr(GEOID,3,5),
+       Tract=substr(GEOID,6,11)) %>%
+  select(!c(state,NAME))
+
+
+
+#Spoke Hub Graphics
+
+
+sub<-hubs.map %>%
+  filter(HUB=="Fifth Season")
+sub.farm<-farms.map %>%
+  filter(HUB=="Fifth Season")
+
+
+ggplot()+
   geom_sf(
-    data = County,
+    data = subset(County, state=="wisconsin" | state=="minnesota"),
     fill=NA, colour="gray",
     size=1,
     inherit.aes=FALSE)+
   geom_sf(
-    data = State,
+    data = subset(State, state=="wisconsin" | state=="minnesota"),
     fill=NA, colour="black",
     size=1,
     inherit.aes=FALSE)+
-  geom_sf(data = farms, 
+  geom_sf(data = sub.farm, 
           size = 1, 
           shape = 23, 
           fill = "darkred")+
-  geom_sf(data = hubs, 
+  geom_sf(data = sub, 
           size = 4, 
-          shape = 10, 
-          fill = "navy") 
+          shape = 25, 
+          fill = "navy") +
+  geom_segment(aes(x = sub$lon1, y = sub$lat1,
+                   xend = sub.farm$lon1, yend = sub.farm$lat1,
+                   colour = "red"))+
+  theme_minimal()+
+  theme(legend.position = "none")
+
+
+ggplot()+
+  geom_sf(data = County,
+    fill=NA, colour="gray",
+    size=1,
+    inherit.aes=FALSE)+
+  geom_sf(data = State,
+    fill=NA, colour="black",
+    size=1,
+    inherit.aes=FALSE)+
+  geom_sf(data = farms.map, 
+          size = 1, 
+          shape = 21, 
+          fill = "darkred")+
+  geom_sf(data = hubs.map, 
+          size = 3, 
+          shape = 25, 
+          fill = "navy")+
+  theme_bw()
+
+
